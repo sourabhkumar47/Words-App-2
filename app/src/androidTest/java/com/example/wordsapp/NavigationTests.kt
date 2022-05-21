c1155e1f95fedb90b2cd0643a68962b32d48bfba
Sourabh Kumar <20010001030@gateway.edu.in>
2022-05-21 22:56:35 +0530

    Added test

--- app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
+++ app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -1,5 +1,47 @@
 package com.example.wordsapp
 
-class NavigationTests {
+import android.app.Application
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.setViewNavController
+import androidx.navigation.testing.TestNavHostController
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.runner.AndroidJUnit4
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
+
+class NavigationTests {
+    @Test
+    fun navigate_to_word_nav_component() {
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+
+        val letterListScenario = launchFragmentInContainer<LetterListFragment>(
+            themeResId =
+            R.style.Theme_Words
+        )
+
+        //declare which navigation graph we want the nav controller to use for the fragment launched.
+
+        letterListScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+        //trigger the event that prompts the navigation.
+        onView(withId(R.id.recycler_view))
+            .perform(RecyclerViewActions
+                .actionOnItemAtPosition<RecyclerView.ViewHolder>(2,click()))
+
+        assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
+
+    }
 }
