29330e65f66c377f08aa339df54cebc1ce9c4741
Sourabh Kumar <20010001030@gateway.edu.in>
2022-09-06 23:37:25 +0530

    Read from the Preferences DataStore

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -2,8 +2,13 @@ package com.example.wordsapp.data
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.flow.map
+import java.util.concurrent.Flow
 import java.util.prefs.Preferences
+
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
 
@@ -14,4 +19,18 @@ private val Context.dataStore : DataStore<Preferences> by preferencesDataStore(
 )
 
 class SettingsDataStore(context: Context) {
+    private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+
+    //Write to the Preferences DataStore
+    suspend fun saveLayoutToPreferencesStore(isLinearLayoutManager: Boolean, context: Context) {
+        context.dataStore.edit { preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
+        }
+    }
+
+    val preferenceFlow: Flow<Boolean> = context.dataStore.data
+        .map {
+            preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] ?: true
+        }
 }
