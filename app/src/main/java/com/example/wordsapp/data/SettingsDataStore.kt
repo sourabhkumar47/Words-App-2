473efc05d892283872e8ee8fb98a0ea2a1d1016a
Sourabh Kumar <20010001030@gateway.edu.in>
2022-09-07 20:52:34 +0530

    fixed SettingsDataStore

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -2,14 +2,15 @@ package com.example.wordsapp.data
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import java.io.IOException
-import java.util.concurrent.Flow
-import java.util.prefs.Preferences
 
 /**
  * Class that handles saving and retrieving layout setting preferences
