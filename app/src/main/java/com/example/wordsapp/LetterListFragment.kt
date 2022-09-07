526cd6ce541f47df0af2c83cd0eb6ee05108f83c
Sourabh Kumar <20010001030@gateway.edu.in>
2022-09-07 20:02:54 +0530

    Read and observe the data

--- app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -16,23 +16,22 @@
 package com.example.wordsapp
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.data.SettingsDataStore
 import com.example.wordsapp.databinding.FragmentLetterListBinding
 
 /**
  * Entry fragment for the app. Displays a [RecyclerView] of letters.
  */
 class LetterListFragment : Fragment() {
+
+    private lateinit var SettingsDataStore = SettingsDataStore
+
     private var _binding: FragmentLetterListBinding? = null
 
     // This property is only valid between onCreateView and
@@ -40,6 +39,7 @@ class LetterListFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var recyclerView: RecyclerView
+
     // Keeps track of which LayoutManager is in use for the [RecyclerView]
     private var isLinearLayoutManager = true
 
@@ -63,7 +63,14 @@ class LetterListFragment : Fragment() {
         recyclerView = binding.recyclerView
         // Sets the LayoutManager of the recyclerview
         // On the first run of the app, it will be LinearLayoutManager
-        chooseLayout()
+//        chooseLayout()
+
+        // Initialize SettingsDataStore
+        SettingsDataStore = SettingsDataStore(requireContext())
+        SettingsDataStore.preferenceFlow.asLiveData().observe(viewLifecycleOwner, { value ->
+            isLinearLayoutManager = value
+            chooseLayout()
+        })
     }
 
     /**
